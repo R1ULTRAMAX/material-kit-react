cb65939065fcfa4ae85c25cc0ec0cad8727a257d
Rudraksh <119955838+R1ULTRAMAX@users.noreply.github.com>
2025-07-08 23:40:53 +0530

    Remove user, product, blog, and sign-in pages

--- src/layouts/nav-config-dashboard.tsx
+++ src/layouts/nav-config-dashboard.tsx
@@ -1,4 +1,3 @@
-import { Label } from 'src/components/label';
 import { SvgColor } from 'src/components/svg-color';
 
 // ----------------------------------------------------------------------
@@ -17,31 +16,6 @@ export const navData = [
     title: 'Dashboard',
     path: '/',
     icon: icon('ic-analytics'),
-  },
-  {
-    title: 'User',
-    path: '/user',
-    icon: icon('ic-user'),
-  },
-  {
-    title: 'Product',
-    path: '/products',
-    icon: icon('ic-cart'),
-    info: (
-      <Label color="error" variant="inverted">
-        +3
-      </Label>
-    ),
-  },
-  {
-    title: 'Blog',
-    path: '/blog',
-    icon: icon('ic-blog'),
-  },
-  {
-    title: 'Sign in',
-    path: '/sign-in',
-    icon: icon('ic-lock'),
   },
   {
     title: 'Not found',
